b3ed99c8e50e24fcfa5b5e9d24b5a6574166f17f
Osama Allabwani <osama.alb96@gmail.com>
2022-10-08 15:27:05 +0200

    Restructured the code to make invoice calculation a separate method and to add a switch for print formats

--- src/main/java/Invoice.java
+++ src/main/java/Invoice.java
@@ -1,3 +1,4 @@
+import java.text.NumberFormat;
 import java.util.*;
 
 public class Invoice {
@@ -5,8 +6,50 @@ public class Invoice {
   public String customer;
   public List<Performance> performances;
 
+  public List<InvoiceItem> invoiceItems;
+
+  public int totalInvoiceAmount;
+
+  public int volumeCredits;
+
   public Invoice(String customer, List<Performance> performances) {
     this.customer = customer;
     this.performances = performances;
+  }
+
+  public void CalculateInvoice(HashMap<String, Play> plays) {
+    this.volumeCredits = 0;
+    this.totalInvoiceAmount = 0;
+    this.invoiceItems = new ArrayList<InvoiceItem>();
+    for (Performance perf : this.performances) {
+      Play play = plays.get(perf.playID);
+      int thisAmount = 0;
+
+      switch (play.type) {
+        case "tragedy":
+          thisAmount = 40000;
+          if (perf.audience > 30) {
+            thisAmount += 1000 * (perf.audience - 30);
+          }
+          break;
+        case "comedy":
+          thisAmount = 30000;
+          if (perf.audience > 20) {
+            thisAmount += 10000 + 500 * (perf.audience - 20);
+          }
+          thisAmount += 300 * perf.audience;
+          break;
+        default:
+          throw new Error("unknown type: ${play.type}");
+      }
+
+      invoiceItems.add(new InvoiceItem(play.name, thisAmount, perf.audience));
+      // add volume credits
+      volumeCredits += Math.max(perf.audience - 30, 0);
+      // add extra credit for every ten comedy attendees
+      if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+
+      totalInvoiceAmount += thisAmount;
+    }
   }
 }
--- src/main/java/StatementPrinter.java
+++ src/main/java/StatementPrinter.java
@@ -7,58 +7,25 @@ public class StatementPrinter {
     HTML
   }
   public String print(Invoice invoice, HashMap<String, Play> plays, printFormat format) {
-    int totalAmount = 0;
-    int volumeCredits = 0;
-    List<InvoiceItem> invoiceItems = new ArrayList<InvoiceItem>();
-    String result = String.format("Statement for %s\n", invoice.customer);
-
-    NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
-
-    for (Performance perf : invoice.performances) {
-      Play play = plays.get(perf.playID);
-      int thisAmount = 0;
-
-      switch (play.type) {
-        case "tragedy":
-          thisAmount = 40000;
-          if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30);
-          }
-          break;
-        case "comedy":
-          thisAmount = 30000;
-          if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
-          }
-          thisAmount += 300 * perf.audience;
-          break;
-        default:
-          throw new Error("unknown type: ${play.type}");
-      }
-
-      invoiceItems.add(new InvoiceItem(play.name, thisAmount, perf.audience));
-      // add volume credits
-      volumeCredits += Math.max(perf.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
-      if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
-
-      // print line for this order
-      result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
-      totalAmount += thisAmount;
+    invoice.CalculateInvoice(plays);
+    switch (format) {
+      case TEXT:
+        return(printText(invoice));
+      case HTML:
+        return(printText(invoice));
+      default:
+        throw new Error("unknown print format: ${format}");
     }
-    result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
-    result += String.format("You earned %s credits\n", volumeCredits);
-    return result;
   }
 
-  public String printText(Invoice invoice, HashMap<String, Play> plays, int totalAmount, int volumeCredits, List<InvoiceItem> invoiceItems) {
+  public String printText(Invoice invoice) {
     String result = String.format("Statement for %s\n", invoice.customer);
     NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
-    for (InvoiceItem invoiceItem : invoiceItems){
+    for (InvoiceItem invoiceItem : invoice.invoiceItems){
       result += String.format("  %s: %s (%s seats)\n", invoiceItem.name, frmt.format(invoiceItem.amount / 100), invoiceItem.audience);
     }
-    result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
-    result += String.format("You earned %s credits\n", volumeCredits);
+    result += String.format("Amount owed is %s\n", frmt.format(invoice.totalInvoiceAmount / 100));
+    result += String.format("You earned %s credits\n", invoice.volumeCredits);
     return result;
   }
 }
